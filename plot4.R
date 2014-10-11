8cbc7212c5d39d34d5cec8e676ed71757c78211e
hazznain <hazznain@hotmail.com>
2014-10-12 01:46:11 +0300

    plt4 axis

--- plot4.R
+++ plot4.R
@@ -3,7 +3,7 @@ data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 select <- data[data$Date == "2007-02-01" | data$Date == "2007-02-02", ]
 timestamp <- paste(as.Date(select$Date), select$Time)
 select$timestamp <- as.POSIXct(timestamp)
-par(mfrow=c(2,2), mar=c(4,4,3,1), oma=c(0,0,0,0))
+par(mfrow=c(2,2), mar=c(4,4,3,2), oma=c(0,0,0,0))
 with(select,{
   
   plot(Global_active_power~timestamp, type="l",
@@ -19,8 +19,8 @@ with(select,{
         legend("topright", col=c("black", "red", "blue"), lty=1, lwd=1, text.width = strwidth("       Sub_metering_2"), 
          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex= 0.8, bty = "n")
   
-  plot(Global_reactive_power~timestamp, type="l", xlab="datetime")
-  axis(side = 2, at= c("0.0","0.1","0.2","0.3","0.4","0.5"))
+  plot(Global_reactive_power~timestamp, type="l", xlab="datetime", cex.axis=0.9)
+  #axis(side = 2, at= c("0.0","0.1","0.2","0.3","0.4","0.5"))
   
 })
 
